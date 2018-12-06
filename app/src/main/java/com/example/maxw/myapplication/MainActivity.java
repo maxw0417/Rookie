3985b3e609d276474afc1be0977b467330d20ea6
maxw <411936452@qq,com>
2018-12-06 21:38:51 +0800

    init

---
+++ app/src/main/java/com/example/maxw/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.maxw.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void initView() {
+
+    }
+
+}
